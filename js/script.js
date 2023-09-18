0d07ba280d0f1194df7ecc9efc68f03120517317
lucio <creppenderman@gmail.com>
2023-09-18 14:56:59 -0300

    implemented resultados()

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 //constantes
+const amplificacion = 5;
+
 const urlParams = new URLSearchParams(window.location.search);
 const r_mueble = JSON.parse(urlParams.get('mueble'));
 const r_color = JSON.parse(urlParams.get('color'));
@@ -12,17 +14,25 @@ const eje2 = document.querySelector("#eje2")
 const eje3 = document.querySelector("#eje3")
 const eje4 = document.querySelector("#eje4")
 
+const respuestas = [r_mueble, r_color, r_animal, r_herramienta, r_instrumento, r_perro]
+
 //variables
 
 
 //funciones
 function resultados(){
     // TODO
-    eje1.style.width = "90%"
-    eje2.style.width = "18%"
-    eje3.style.width = "64%"
-    eje4.style.width = "2%"
-    
+    for (r of respuestas){
+        eje1.innerText = parseInt(eje1.style.width) + r[0]*amplificacion + "%"
+        eje2.innerText = parseInt(eje2.style.width) + r[1]*amplificacion + "%"
+        eje3.innerText = parseInt(eje3.style.width) + r[2]*amplificacion + "%"
+        eje4.innerText = parseInt(eje4.style.width) + r[3]*amplificacion + "%"
+
+        eje1.style.width = parseInt(eje1.style.width) + r[0]*amplificacion + "%"
+        eje2.style.width = parseInt(eje2.style.width) + r[1]*amplificacion + "%"
+        eje3.style.width = parseInt(eje3.style.width) + r[2]*amplificacion + "%"
+        eje4.style.width = parseInt(eje4.style.width) + r[3]*amplificacion + "%"
+    }
 }
 
 
